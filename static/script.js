99056ecd54b7eea778661d2cf838b5d001f8f7a1
Marley <mpsvideos.marley@gmail.com>
2025-02-12 18:20:54 -0300

    ajustes na página script.js

--- static/script.js
+++ static/script.js
@@ -10,18 +10,15 @@ menuMobile.addEventListener('click', () => {
     body.classList.toggle('menu-nav-active');
 });
 
-// Fecha o menu quando clicar em um item do menu
-const navItem = document.querySelectorAll('.nav-item');
-navItem.addEventListener('click', () => {
-    menuMobile.classList.replace('bi-x', 'bi-list');
-    body.classList.remove('menu-nav-active');
-});
 
-navItem.forEach((item) => {
+// Fecha o menu quando clicar em algum item e muda o ícone para list
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item => {
     item.addEventListener('click', () => {
         if (body.classList.contains('menu-nav-active')) {
             body.classList.remove('menu-nav-active');
             menuMobile.classList.replace('bi-x', 'bi-list');
         }
-    });
-});
+    })
+})
